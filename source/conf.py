2849427e04f8ea1563d0ce2922e13a667be9664a
yehua517 <xuwei@xuweideMacBook-Pro.local>
2017-06-10 14:28:17 +0800

    修改配置

--- source/conf.py
+++ source/conf.py
@@ -76,6 +76,7 @@ pygments_style = 'sphinx'
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 
+latex_keep_old_macro_names=False
 
 # -- Options for HTML output ----------------------------------------------
 
